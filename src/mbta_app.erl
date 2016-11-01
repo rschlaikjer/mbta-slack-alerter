8c787554e2a4480d04a3e2732fdfb459f4a71477
Ross Schlaikjer <ross@schlaikjer.net>
2016-11-01 00:04:56 -0400

    Green line IDs are per-line

--- src/mbta_app.erl
+++ src/mbta_app.erl
@@ -14,7 +14,10 @@ start(_StartType, _StartArgs) ->
     mbta:compile_protobuf(),
     {ok, Pid} = mbta_sup:start_link(),
     mbta:start_line("Red", <<"#E23139">>),
-    mbta:start_line("Green", <<"#019362">>),
+    mbta:start_line("Green-B", <<"#019362">>),
+    mbta:start_line("Green-C", <<"#019362">>),
+    mbta:start_line("Green-D", <<"#019362">>),
+    mbta:start_line("Green-E", <<"#019362">>),
     mbta:start_line("Blue", <<"#007FC5">>),
     mbta:start_line("Orange", <<"#F7941D">>),
     {ok, Pid}.
